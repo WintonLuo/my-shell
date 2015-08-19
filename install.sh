489d419efa7aacfe623cca51a1050850cf7ac8e6
Heaven1881 <Heaven1881@163.com>
2015-08-19 10:28:34 +0800

    change clone url

--- install.sh
+++ install.sh
@@ -15,7 +15,10 @@ cp vimrc ~/.vimrc
 # 插件列表
 
 VIMRC_FILE="vimrc"
-hostname="https://github.com/"
+hostname="git://github.com/"
+echo -e "\033[32m[INFO]\033[0m Install vim plugin..."
+mkdir -p ~/.vim/bundle
+
 cat ${VIMRC_FILE} | while read line
 do
     plugin_line=$(echo $line | awk -F " " '{print $1}')
@@ -24,9 +27,8 @@ do
         plugin=${plugin%?}
         url="${hostname}"$(echo ${plugin})".git"
 
-        cd ~/.vim/bundle/
         echo -e "\033[36mPlugin: ${url}\033[0m"
-        git clone ${url}
+        cd ~/.vim/bundle/ && git clone ${url}
     fi
 done
 
